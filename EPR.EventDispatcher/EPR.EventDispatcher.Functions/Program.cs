3cf71554ba238d6881ffa7e949c4c3cb4c044feb
= <mark.john.jones@esynergy.co.uk>
2026-05-29 10:24:40 +0100

    Convert program class to top-level code configuration to avoid Sonar warning without using the ExcludeFromCodeCoverage attribute

--- EPR.EventDispatcher/EPR.EventDispatcher.Functions/Program.cs
+++ EPR.EventDispatcher/EPR.EventDispatcher.Functions/Program.cs
@@ -1,46 +1,38 @@
-﻿namespace EPR.EventDispatcher.Functions;
-
-using System.Diagnostics.CodeAnalysis;
+﻿#pragma warning disable SA1200
 using Azure.Messaging.EventHubs.Producer;
-using Application.Services;
-using Application.Services.Interfaces;
-using Config;
-using Extensions;
+using EPR.EventDispatcher.Application.Services;
+using EPR.EventDispatcher.Application.Services.Interfaces;
+using EPR.EventDispatcher.Functions.Config;
+using EPR.EventDispatcher.Functions.Extensions;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
+#pragma warning restore SA1200
 
-[ExcludeFromCodeCoverage]
-public static class Program
-{
-    public static async Task Main()
+var host = new HostBuilder()
+    .ConfigureFunctionsWorkerDefaults()
+    .ConfigureServices((context, services) =>
     {
-        var host = new HostBuilder()
-            .ConfigureFunctionsWorkerDefaults()
-            .ConfigureServices((context, services) =>
+        services
+            .AddApplicationInsightsTelemetryWorkerService()
+            .ConfigureFunctionsApplicationInsights()
+            .ConfigureOptions();
+
+        if (context.HostingEnvironment.IsDevelopment())
+        {
+            services.AddSingleton<IEventService, EventServiceLocal>();
+        }
+        else
+        {
+            services.AddSingleton<IEventService, EventService>();
+            services.AddSingleton(sp =>
             {
-                services
-                    .AddApplicationInsightsTelemetryWorkerService()
-                    .ConfigureFunctionsApplicationInsights()
-                    .ConfigureOptions();
+                var options = sp.GetRequiredService<IOptions<EventDispatcherOptions>>().Value;
+                return new EventHubProducerClient(options.ConnectionString, options.EventHubName);
+            });
+        }
+    })
+    .Build();
 
-                if (context.HostingEnvironment.IsDevelopment())
-                {
-                    services.AddSingleton<IEventService, EventServiceLocal>();
-                }
-                else
-                {
-                    services.AddSingleton<IEventService, EventService>();
-                    services.AddSingleton(sp =>
-                    {
-                        var options = sp.GetRequiredService<IOptions<EventDispatcherOptions>>().Value;
-                        return new EventHubProducerClient(options.ConnectionString, options.EventHubName);
-                    });
-                }
-            })
-            .Build();
-
-        await host.RunAsync();
-    }
-}
+await host.RunAsync();
